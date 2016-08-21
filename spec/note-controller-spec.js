fdf1e41e48d9746b2d0d1d80d4602c0c9c13afe8
Richard Bates <richo225@gmail.com>
2016-08-21 20:40:18 +0100

    Refactor/nest note-controller tests

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -6,16 +6,11 @@ function setupModel(){
   controller = new NoteController(view);
 }
 
-describe("NoteController", function(){
+describe("Note controller", function(){
 
   it("controller can be instantiated", function(){
     setupModel();
     isTrue(controller.view === view);
-  });
-
-  it("HTML for an empty note list is displayed when app loads", function(){
-    mockElement("list");
-    isTrue(appElement.innerHTML === undefined);
   });
 
   it("form submit creates a note with text from input", function(){
@@ -25,26 +20,39 @@ describe("NoteController", function(){
     isTrue(list.notes[0].getText() === "This is my first note");
   });
 
-  it("HTML for new note list is displayed when form submitted", function(){
-    setupModel();
-    mockElement("list");
-    unstubForm();
-    controller.formSubmit("This is my first note");
-    isTrue(view.returnList() === appElement.innerHTML);
+  describe("list HTML", function(){
+
+    it("HTML for an empty note list is displayed when app loads", function(){
+      mockElement("list");
+      isTrue(appElement.innerHTML === undefined);
+    });
+
+    it("HTML for new note list is displayed when form submitted", function(){
+      setupModel();
+      mockElement("list");
+      unstubForm();
+      controller.formSubmit("This is my first note");
+      isTrue(view.returnList() === appElement.innerHTML);
+    });
+
   });
 
-  it("HTML for an empty single note is displayed when app loads", function(){
-    mockElement("note");
-    isTrue(appElement.innerHTML === undefined);
-  });
+  describe("note HTML", function(){
 
-  it("HTML for single note is displayed when clicked", function(){
-    setupModel();
-    mockElement("note");
-    controller.formSubmit("This is my first note");
-    mockHash("0");
-    controller.showNote();
-    isTrue(appElement.innerHTML === "<div>This is my first note</div>");
+    it("HTML for an empty single note is displayed when app loads", function(){
+      mockElement("note");
+      isTrue(appElement.innerHTML === undefined);
+    });
+
+    it("HTML for single note is displayed when clicked", function(){
+      setupModel();
+      mockElement("note");
+      controller.formSubmit("This is my first note");
+      mockHash("0");
+      controller.showNote();
+      isTrue(appElement.innerHTML === "<div>This is my first note</div>");
+    });
+
   });
 
 });
