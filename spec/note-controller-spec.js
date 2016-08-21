5bbc1c6700b52607f92728efc767c636b199f6a8
Richard Bates <richo225@gmail.com>
2016-08-21 19:54:39 +0100

    Added the last few note-controller tests

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -13,6 +13,12 @@ function mockElement(element){
   };
 }
 
+function mockHash(hash){
+  controller.getIdByHash = function(){
+    return hash;
+  };
+}
+
 function stubForm(){
   NoteController.prototype.formSubmit = function(submitText){
     this.view.noteListModel.saveNote(submitText);
@@ -33,19 +39,38 @@ describe("NoteController", function(){
     isTrue(controller.view === view);
   });
 
-  it("submit creates a note with text from input", function(){
+  it("HTML for an empty note list is displayed when app loads", function(){
+    mockElement("list");
+    isTrue(appElement.innerHTML === undefined);
+  });
+
+  it("form submit creates a note with text from input", function(){
     setupModel();
     stubForm();
     controller.formSubmit("This is my first note");
     isTrue(list.notes[0].getText() === "This is my first note");
   });
 
-  it("submitted note has HTML inserted into list", function(){
+  it("HTML for new note list is displayed when form submitted", function(){
     setupModel();
     mockElement("list");
     unstubForm();
     controller.formSubmit("This is my first note");
     isTrue(view.returnList() === appElement.innerHTML);
+  });
+
+  it("HTML for an empty single note is displayed when app loads", function(){
+    mockElement("note");
+    isTrue(appElement.innerHTML === undefined);
+  });
+
+  it("HTML for single note is displayed when clicked", function(){
+    setupModel();
+    mockElement("note");
+    controller.formSubmit("This is my first note");
+    mockHash("0");
+    controller.showNote();
+    isTrue(appElement.innerHTML === "<div>This is my first note</div>");
   });
 
 });
